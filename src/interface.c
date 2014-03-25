c486fe6a87b8f797a008eccd7f3043d2ceffbbcf
Martin Robinson <martinjrobins@gmail.com>
2014-03-25 11:59:56 +0000

    fixed resolution for  number fields for epsilon, kappa, etc

--- src/interface.c
+++ src/interface.c
@@ -361,7 +361,7 @@ gboolean dialog (
 //
 //  gtk_widget_show (table);
 
-  interface_vals.threshold_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 4,"_Mask Threshold:", SCALE_WIDTH, 0,vals->threshold, 0, 255, 0.001, 0.1, EPS_DIGITS,TRUE, 0, 0,NULL, NULL);
+  interface_vals.threshold_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 4,"_Mask Threshold:", SCALE_WIDTH, 0,vals->threshold, 0, 255, 0.1, 0.2, EPS_DIGITS,TRUE, 0, 0,NULL, NULL);
    g_signal_connect (interface_vals.threshold_scale, "value_changed", 	G_CALLBACK(dialogThresholdChanged), vals);
 
    GtkWidget *separator = gtk_hseparator_new ();
@@ -377,10 +377,10 @@ gboolean dialog (
    gtk_widget_show (table);
 
 
-  interface_vals.epsilon_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 0,"_Pixel neighborhood (epsilon):", SCALE_WIDTH, 0,vals->epsilon, 1, SCALE_MAX, 0.001, 0.1, EPS_DIGITS,TRUE, 0, 0,NULL, NULL);
+  interface_vals.epsilon_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 0,"_Pixel neighborhood (epsilon):", SCALE_WIDTH, 0,vals->epsilon, 1, SCALE_MAX, 0.1, 0.2, EPS_DIGITS,TRUE, 0, 0,NULL, NULL);
   g_signal_connect (interface_vals.epsilon_scale, "value_changed", 	G_CALLBACK(gimp_float_adjustment_update), &vals->epsilon);
 
-  interface_vals.kappa_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 1, "_Sharpness (kappa in %):", SCALE_WIDTH, 0,	vals->kappa, 0, CONV_MAX, 0.001, 0.1, KAPPA_DIGITS,TRUE, 0, 0,NULL, NULL);
+  interface_vals.kappa_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 1, "_Sharpness (kappa in %):", SCALE_WIDTH, 0,	vals->kappa, 0, CONV_MAX, 0.01, 0.1, KAPPA_DIGITS,TRUE, 0, 0,NULL, NULL);
   g_signal_connect (interface_vals.kappa_scale, "value_changed", 	G_CALLBACK(gimp_float_adjustment_update), &vals->kappa);
 
   interface_vals.sigma_scale = gimp_scale_entry_new (GTK_TABLE (table), 0, 2, "_Pre-smoothing (sigma):", SCALE_WIDTH, 0,	vals->sigma, 0, SCALE_MAX, 0.001, 0.1, SMOOTH_DIGITS,TRUE, 0, 0,NULL, NULL);
